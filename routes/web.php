94d6ad716308ebfd24112aaa4e7ccd240140cb0f
Shane Poppleton <shane@bjja.com.au>
2022-06-14 11:12:33 +1000

    Removed main route

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,3 @@ use App\Http\Middleware\ValidateMailgun;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(ValidateMailgun::class)->post('/report', ReportController::class);
-
-
-Route::get('/', function() {
-   return "Hello World";
-});
